e459d80012d0dd4c5bdc7fd7595a79d307404728
SAMANTHA YU <YUS@LAFAYETTE.EDU>
2015-05-12 09:34:49 -0400

    Allows drawings to be saved with proper file type

--- HeatMap_v2_Save_Drawings.tcl
+++ HeatMap_v2_Save_Drawings.tcl
@@ -1,9 +1,9 @@
 proc set_drawing_location {} {
-  
+
   global fileSaveLocation
 
   set fileSaveLocation [tk_chooseDirectory -title "Where should the files be saved?" ]
-  
+
   if {$fileSaveLocation eq ""} {
     set fileSaveLocation "choose output file location"
   }
@@ -20,10 +20,10 @@ proc save_all_drawing {} {
   global canD
   global canP
   #global canC
-  global drawingWindow 
+  global drawingWindow
   global currentMaze
 
- 
+
   foreach ref $orderedNames {
     if { $loadedFileVis($ref) eq "true" } {
 
@@ -44,17 +44,18 @@ puts "all drawings are saved"
 }
 
 proc save_drawing {can fileInfo} {
-  
+
   global fileSaveLocation
   global can_width
   global can_height
-  
+  global drawingFileType
+
   if {$fileSaveLocation eq "choose output file location"} {
     tk_messageBox -message "You must first select a location and file name."
     return
   }
-  
-  set fileSaveName "$fileSaveLocation/$fileInfo.gif"
+
+  set fileSaveName "$fileSaveLocation/$fileInfo.$drawingFileType"
 
   $can postscript -file $fileSaveName -width [expr $can_width*3] -height [expr $can_height * 3]
 }
