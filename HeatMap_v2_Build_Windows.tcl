21fcaade11b7b1ed5536f9b5f73ebf96066dc026
SAMANTHA YU <YUS@LAFAYETTE.EDU>
2015-05-12 09:36:39 -0400

    Gives postscript files proper file ending

--- HeatMap_v2_Build_Windows.tcl
+++ HeatMap_v2_Build_Windows.tcl
@@ -1,8 +1,8 @@
 proc build_drawing_windows {} {
- 
-	build_zTime_drawing_window 
-	build_zDist_drawing_window 
-	build_PeskyEff_drawing_window  
+
+	build_zTime_drawing_window
+	build_zDist_drawing_window
+	build_PeskyEff_drawing_window
 	build_color_spectrum
 }
 
@@ -14,7 +14,7 @@ proc build_zTime_drawing_window {} {
 	global canT
 	global drawingWindow
 
-	set canT 0  
+	set canT 0
 	array set drawingWindow {}
 
 	global drawingFileType
@@ -31,15 +31,15 @@ proc build_zTime_drawing_window {} {
 			incr num_of_trials
 		}
 	}
-  
+
 	toplevel .zTimeWindow
-	wm title .zTimeWindow "Heat Map using z-Scores for Time"   
+	wm title .zTimeWindow "Heat Map using z-Scores for Time"
 
 	set t2 ".zTimeWindow"
 
 	#--- frame one (button to source outside files for data)
 	frame $t2.f1 -width 0
-	pack  $t2.f1 -side top -anchor nw -fill x 
+	pack  $t2.f1 -side top -anchor nw -fill x
 
 	set fileSaveLocation "choose output file location"
 	button $t2.f1.b3 -command set_drawing_location -textvar fileSaveLocation
@@ -52,12 +52,12 @@ proc build_zTime_drawing_window {} {
 	button $t2.f2.b2 -command save_all_drawing -text "save drawing" -width 20
 	pack   $t2.f2.b2 -side left -anchor nw  -fill x
 
-	set combolist  [list postscript jpg png]
+	set combolist  [list ps jpg png]
 	set drawingFileType [lindex $combolist 0 ]
 	ttk::combobox $t2.f2.cb1 -values $combolist -textvar drawingFileType
 	pack     $t2.f2.cb1 -side left -anchor nw  -fill x
 
-	#--- frame three (refresh button and what maze number the 
+	#--- frame three (refresh button and what maze number the
 	#--- data is from / should be shown)
 	frame $t2.f3 -width 0
 	pack  $t2.f3 -side top -anchor nw -fill x
@@ -77,11 +77,11 @@ proc build_zTime_drawing_window {} {
 
 	#--- frame four (display all maze maps)
 	frame $t2.f4 -width 0 -relief groove -borderwidth 0
-	pack  $t2.f4 -side top -expand 0 -pady 20 
+	pack  $t2.f4 -side top -expand 0 -pady 20
 
 	frame $t2.f5 -width 0 -relief groove -borderwidth 0
-	pack  $t2.f5 -side bottom -expand 0 -pady 20 
- 
+	pack  $t2.f5 -side bottom -expand 0 -pady 20
+
 	foreach ref $orderedNames {
 
 		if { $loadedFileVis($ref) eq "true" } {
@@ -92,7 +92,7 @@ proc build_zTime_drawing_window {} {
 				set drawingWindow($trial\_canT)              \
 					[canvas $t2.f4.drawingWindow($trial\_canT) \
 							-width $can_width                      \
-							-height $can_height                      ] 
+							-height $can_height                      ]
 
 			} else {
 
@@ -101,18 +101,18 @@ proc build_zTime_drawing_window {} {
 					set drawingWindow($trial\_canT) [           \
 						canvas $t2.f4.drawingWindow($trial\_canT) \
 							-width $can_width                       \
-							-height $can_height] 
+							-height $can_height]
 
 				} elseif { $trial > ($num_of_trials / 2) } {
 
 					set drawingWindow($trial\_canT) [            \
 						canvas $t2.f5.drawingWindow($trial\_canT)  \
 						-width $can_width                          \
-						-height $can_height]   
-				} 
+						-height $can_height]
+				}
 			}
 
-			pack $drawingWindow($trial\_canT) -side left -anchor nw 
+			pack $drawingWindow($trial\_canT) -side left -anchor nw
 		}
 	}
 }
@@ -124,13 +124,13 @@ proc build_zDist_drawing_window {} {
 	global canD
 	global drawingWindow
 
-	set canD 0  
+	set canD 0
 
 	array set drawingWindow {}
 
 	global prevTD
 
-	set prevTD 0 
+	set prevTD 0
 
 	global drawingFileType
 	global fileSaveLocation
@@ -151,13 +151,13 @@ proc build_zDist_drawing_window {} {
 	}
 
 	toplevel .zDistWindow
-	wm title .zDistWindow "Heat Map using z-Scores for Distance"   
+	wm title .zDistWindow "Heat Map using z-Scores for Distance"
 
 	set t3 ".zDistWindow"
 
 	#--- frame one (button to source outside files for data)
 	frame $t3.f1 -width 0
-	pack  $t3.f1 -side top -anchor nw -fill x 
+	pack  $t3.f1 -side top -anchor nw -fill x
 
 	set fileSaveLocation "choose output file location"
 	button $t3.f1.b3 -command set_drawing_location -textvar fileSaveLocation
@@ -170,14 +170,14 @@ proc build_zDist_drawing_window {} {
 	button $t3.f2.b2 -command save_all_drawing -text "save drawing" -width 20
 	pack   $t3.f2.b2 -side left -anchor nw  -fill x
 
-	set combolist  [list postscript jpg png]
+	set combolist  [list ps jpg png]
 	set drawingFileType [lindex $combolist 0 ]
 
 	ttk::combobox $t3.f2.cb1 -values $combolist -textvar drawingFileType
 
 	pack $t3.f2.cb1 -side left -anchor nw  -fill x
 
-	#--- frame three (refresh button and what maze number 
+	#--- frame three (refresh button and what maze number
 	#--- the data is from / should be shown)
 	frame $t3.f3 -width 0
 	pack  $t3.f3 -side top -anchor nw -fill x
@@ -198,10 +198,10 @@ proc build_zDist_drawing_window {} {
 
 	#--- frame four (display all maze maps)
 	frame $t3.f4 -width 0 -relief groove -borderwidth 0
-	pack  $t3.f4 -side top -expand 0 -pady 20 
+	pack  $t3.f4 -side top -expand 0 -pady 20
 
 	frame $t3.f5 -width 0 -relief groove -borderwidth 0
-	pack  $t3.f5 -side bottom -expand 0 -pady 20 
+	pack  $t3.f5 -side bottom -expand 0 -pady 20
 
 	foreach ref $orderedNames {
 
@@ -218,13 +218,13 @@ proc build_zDist_drawing_window {} {
 			} else {
 
 				if { $trial <= ($num_of_trials / 2) } {
-					set drawingWindow($trial\_canD) [canvas $t3.f4.drawingWindow($trial\_canD)  -width $can_width -height $can_height] 
+					set drawingWindow($trial\_canD) [canvas $t3.f4.drawingWindow($trial\_canD)  -width $can_width -height $can_height]
 				} elseif { $trial > ($num_of_trials / 2) } {
-					set drawingWindow($trial\_canD) [canvas $t3.f5.drawingWindow($trial\_canD)  -width $can_width -height $can_height]   
-				} 
+					set drawingWindow($trial\_canD) [canvas $t3.f5.drawingWindow($trial\_canD)  -width $can_width -height $can_height]
+				}
 			}
 
-			pack $drawingWindow($trial\_canD) -side left -anchor nw 
+			pack $drawingWindow($trial\_canD) -side left -anchor nw
 		}
 	}
 }
@@ -236,11 +236,11 @@ proc build_PeskyEff_drawing_window {} {
 	global canP
 	global drawingWindow
 
-	set canP 0  
+	set canP 0
 	array set drawingWindow {}
 
 	global prevTP
-	set prevTP 0 
+	set prevTP 0
 
 	global drawingFileType
 	global fileSaveLocation
@@ -259,13 +259,13 @@ proc build_PeskyEff_drawing_window {} {
 	}
 
 	toplevel .peskyEffWindow
-	wm title .peskyEffWindow "Heat Map using Pesky Efficiency Scores"   
+	wm title .peskyEffWindow "Heat Map using Pesky Efficiency Scores"
 
 	set t4 ".peskyEffWindow"
 
 	#--- frame one (button to source outside files for data)
 	frame $t4.f1 -width 0
-	pack  $t4.f1 -side top -anchor nw -fill x 
+	pack  $t4.f1 -side top -anchor nw -fill x
 
 	set fileSaveLocation "choose output file location"
 	button $t4.f1.b3 -command set_drawing_location -textvar fileSaveLocation
@@ -278,7 +278,7 @@ proc build_PeskyEff_drawing_window {} {
 	button $t4.f2.b2 -command save_all_drawing -text "save drawing" -width 20
 	pack   $t4.f2.b2 -side left -anchor nw  -fill x
 
-	set combolist  [list postscript jpg png]
+	set combolist  [list ps jpg png]
 	set drawingFileType [lindex $combolist 0 ]
 	ttk::combobox $t4.f2.cb1 -values $combolist -textvar drawingFileType
 	pack     $t4.f2.cb1 -side left -anchor nw  -fill x
@@ -298,10 +298,10 @@ proc build_PeskyEff_drawing_window {} {
 
 	#--- frame four (display all maze maps)
 	frame $t4.f4 -width 0 -relief groove -borderwidth 0
-	pack  $t4.f4 -side top -expand 0 -pady 20 
+	pack  $t4.f4 -side top -expand 0 -pady 20
 
 	frame $t4.f5 -width 0 -relief groove -borderwidth 0
-	pack  $t4.f5 -side bottom -expand 0 -pady 20 
+	pack  $t4.f5 -side bottom -expand 0 -pady 20
 
 	foreach ref $orderedNames {
 
@@ -314,7 +314,7 @@ proc build_PeskyEff_drawing_window {} {
 				set drawingWindow($trial\_canP) [            \
 					canvas $t4.f4.drawingWindow($trial\_canP)  \
 						-width $can_width                        \
-						-height $can_height] 
+						-height $can_height]
 
 			} else {
 
@@ -330,11 +330,11 @@ proc build_PeskyEff_drawing_window {} {
 					set drawingWindow($trial\_canP) [            \
 						canvas $t4.f5.drawingWindow($trial\_canP)  \
 							-width $can_width                        \
-							-height $can_height]   
-				} 
+							-height $can_height]
+				}
 			}
 
-			pack $drawingWindow($trial\_canP) -side left -anchor nw 
+			pack $drawingWindow($trial\_canP) -side left -anchor nw
 		}
 	}
 }
@@ -347,9 +347,9 @@ proc build_color_spectrum {} {
 	global canC
 
 	toplevel .s
-	wm title .s "Color Spectrum"   
-  
-	set t5 ".s" 
+	wm title .s "Color Spectrum"
+
+	set t5 ".s"
 
 	frame $t5.f1 -width 0
 	pack  $t5.f1 -side left -anchor nw  -fill x -expand 0
@@ -363,8 +363,8 @@ proc build_color_spectrum {} {
 		set redVal    [expr int( 65535 - (abs($c / 2) * 65535) )]
 		set cgreenVal [expr int( 65535 - (abs($c / 2) * 45535) )]
 		set blueVal   29
-		set colorVal  [format "#%04x%04x%04x" $redVal $cgreenVal $blueVal ]       
-		
+		set colorVal  [format "#%04x%04x%04x" $redVal $cgreenVal $blueVal ]
+
 		$t5.f1.canC  create rectangle [expr 2*(215 - $cg)] 0 [expr 2*(200 - ($cg-10))] 100  -fill $colorVal -outline $colorVal
 		if {$cg % 25 == 0 && $cg != 0} {
 			$t5.f1.canC  create text [expr 2*(210 - $cg)] 100 -text "[expr -1 * $c]" -anchor nw
@@ -377,12 +377,12 @@ proc build_color_spectrum {} {
 		set redVal    65535
 		set greenVal  [expr {int( 65535.0 - (1.0 * $d)  / 2 * 65535.0 )}]
 		set blueVal   29
-		set colorVal  [format "#%04x%04x%04x" $redVal $greenVal $blueVal ]       
+		set colorVal  [format "#%04x%04x%04x" $redVal $greenVal $blueVal ]
 
 		$t5.f1.canC  create rectangle [expr 2*($cr + 215)] 0 [expr 2*($cr + 220)] 100 -fill $colorVal -outline $colorVal
 
 		if {$cr % 25 == 0} {
 			$t5.f1.canC  create text [expr 2*($cr +213)] 100 -text  "$d" -anchor nw
-		} 
+		}
 	}
 }
