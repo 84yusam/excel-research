5cb4b682aef265c6bd479c7021fa1da1038d6d1b
84yusam <yus@lafayette.edu>
2015-06-29 09:02:30 -0400

    Create directory_tools_v5.tcl

---
+++ directory_tools_v5.tcl
@@ -0,0 +1,33 @@
+proc split_data_file { str } {
+
+  global current_maze_dir
+
+  if {[ regexp {^(.+)\_(.+)\_(.+)\-(.+)\-(.+)\-(\d+)\.(\d+)\.txt$} $str match \
+            dir_date dir_time dir_iteration\
+            maze_number maze_iteration \
+            file_date file_time ]} {
+
+    # puts $dir_date
+    # puts $dir_time
+    # puts $dir_iteration
+    # puts $maze_number
+    # puts $maze_iteration
+    # puts $file_date
+    # puts $file_time
+    # puts "------"
+    set current_maze_dir(dir_date)       $dir_date
+    set current_maze_dir(dir_time)       $dir_time
+    set current_maze_dir(dir_iteration)  $dir_iteration
+    set current_maze_dir(maze)           $maze_number
+    set current_maze_dir(maze_iteration) $maze_iteration
+    set current_maze_dir(file_date)      $file_date
+    set current_maze_dir(file_time)      $file_time
+
+    return 1
+
+  } else {
+
+    return 0
+
+  }
+}
