6783a8a4b4e106fd33f23be55bc32c5cdff6beeb
SAMANTHA YU <YUS@LAFAYETTE.EDU>
2015-07-07 12:30:17 -0400

    Save imgs. Incorrect drawings & indiv. not working

--- build_windows_v5.tcl
+++ build_windows_v5.tcl
@@ -1,6 +1,14 @@
 proc build_window { maze id_folder num_trials } {
   global can_width
   global can_height
+  global chosentrial
+
+  set triallist [list "Choose a trial"]
+  set chosentrial "Choose a trial"
+
+  for {set i 1} {$i <= $num_trials} {incr i} {
+    lappend triallist $i
+  }
 
   set folderName [file tail $id_folder]
   set id [split_id $folderName]
@@ -17,7 +25,17 @@ proc build_window { maze id_folder num_trials } {
   pack   $t1.f1.b1 -side top -anchor nw -fill x -expand true
 
   frame $t1.f2 -width 0
-  pack  $t1.f2 -side top -anchor nw -fill both -expand true
+  pack  $t1.f2 -side top -anchor nw -fill x
+
+  button $t1.f2.b1 -text "Save individual drawing"
+  pack   $t1.f2.b1 -side left -anchor nw
+
+  ttk::combobox $t1.f2.cb -values $triallist -textvar chosentrial
+  pack          $t1.f2.cb -side left -anchor nw -fill x
+  $t1.f2.cb current 0
+
+  frame $t1.f3 -width 0
+  pack  $t1.f3 -side top -anchor nw -fill both -expand true
 
   if {$num_trials == 1} {
     set maincanwidth $can_width
@@ -33,10 +51,11 @@ proc build_window { maze id_folder num_trials } {
     set maincanheight [expr $can_height]
   }
 
-  set  maincanvas [canvas $t1.f2.maincan -bg black -width $maincanwidth -height $maincanheight]
+  set  maincanvas [canvas $t1.f3.maincan -bg black -width $maincanwidth -height $maincanheight]
   pack $maincanvas -side top -anchor nw -expand false
 
   $t1.f1.b1 configure -command [list save_all_drawings $maincanvas $maincanheight $maincanwidth $id $maze]
+  $t1.f2.b1 configure -command [list save_individual_drawing $maincanvas $chosentrial $id $maze]
 
   return $maincanvas
 }
@@ -48,24 +67,26 @@ proc process_trial_canvas { maincan num_trials trial_cnt file_name } {
   global can_height
   global current_maze_dir
 
+  set current_trial [expr $trial_cnt + 1]
+
   split_data_file [file tail $file_name]
 
-  set subcan($trial_cnt) [canvas $maincan.can$trial_cnt -height $can_height -width $can_width]
+  set subcan($current_trial) [canvas $maincan.can$current_trial -height $can_height -width $can_width]
 
   #choose row based on the trial number of the canvas
-  if {$current_maze_dir(maze_iteration) <= 3} {
-    set xLoc [expr [expr $current_maze_dir(maze_iteration) - 1]*$can_width]
+  if {$current_trial <= 3} {
+    set xLoc [expr $trial_cnt*$can_width]
     set yLoc 0
   } else {
-    set xLoc [expr [expr $current_maze_dir(maze_iteration) - 4]*$can_width]
+    set xLoc [expr [expr $trial_cnt - 3]*$can_width]
     set yLoc $can_height
   }
 
-  set win$trial_cnt [$maincan create window $xLoc $yLoc -width $can_width -height $can_height -anchor nw -window $subcan($trial_cnt)]
-  $subcan($trial_cnt) create text 125 300 \
+  set win$current_trial [$maincan create window $xLoc $yLoc -width $can_width -height $can_height -anchor nw -window $subcan($current_trial)]
+  $subcan($current_trial) create text 125 300 \
                       -text "ID: $current_maze_dir(dir_date)$current_maze_dir(dir_time)$current_maze_dir(dir_iteration) \
                       $current_maze_dir(maze) Trial $current_maze_dir(maze_iteration)"
 
-  return $subcan($trial_cnt)
+  return $subcan($current_trial)
 
 }
--- save_images_v5.tcl
+++ save_images_v5.tcl
@@ -1,16 +1,34 @@
 proc save_all_drawings { maincan can_height can_width id maze} {
 
   set saveloc [tk_chooseDirectory -title "Where should the file be saved?" -mustexist true]
-  set filename "$saveloc/allimages$id$maze"
+  set imgtype "allimages"
+  set filename "$saveloc/$id$maze$imgtype"
 
   if {$saveloc eq ""} {
     tk_messageBox -message "Please choose a valid location."
   } else {
     $maincan postscript -file $filename -width $can_width -height $can_height
+    tk_messageBox -message "File saved."
   }
-
 }
 
-proc save_individual_drawing {} {
+proc save_individual_drawing {maincan trial id maze} {
+  global can_height
+  global can_width
+
+  if {$trial eq "Choose a trial"} {
+    tk_messageBox -message "Please choose a trial."
+  } else {
+    set saveloc [tk_chooseDirectory -title "Where should the file be saved?" -mustexist true]
+    set imgtype "trial$trial"
+    set filename "$saveloc/$id$maze$imgtype"
+
+    if {$saveloc eq ""} {
+      tk_messageBox -message "Please choose a valid location."
+    } else {
+      $maincan.can$trial postscript -file $filename -width $can_width -height $can_height
+      tk_messageBox -message "File saved."
+    }
+  }
 
 }
